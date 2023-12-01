1e75b3618031e58434379b937f3a0cae50262f10
mamps23 <priya.mirdha@kyndryl.com>
2023-12-01 13:14:17 +0530

    First script

---
+++ first.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#To check what type of file,directory,inode no. of files and directory
+
+echo -e $GREEN " First method"
+
+stat /c/Users/PriyaMirdha/Practice/file*
+stat /c/Users/PriyaMirdha/Practice
+
+
+echo " Second way of doing "
+
+egrep "file1|file2|file3" | ls -li
+
+
+
+echo " 3rd way "
+
+Take_file="$1"
+
+ls -li "$Take_file"
+
+
+
